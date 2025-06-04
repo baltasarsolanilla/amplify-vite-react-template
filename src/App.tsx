6ab2c0e769e7f6bd91eaa35639cdb9c88f6594ee
Balti Solanilla <solanilla.baltasar@gmail.com>
2025-06-03 17:23:21 -0700

    dev env try out

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos DEV ENV</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
